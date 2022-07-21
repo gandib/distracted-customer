973f10f803e70d7817808a7332829046111f76fe
Gandib Roy <gandibroy11@gmail.com>
2022-07-21 19:06:21 +0600

    First commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    const [randomProduct, setRandomProduct] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
+    const handleAddToCart = (selectedProduct) => {
+        let newCart = [];
+        let value = cart.length;
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if (!exists) {
+            if (value < 4) {
+                newCart = [...cart, selectedProduct];
+            }
+            else {
+                alert("Oops... Can't selected more than 4!");
+                newCart = [...cart];
+            }
+        }
+        else {
+            alert('Product already added.')
+            newCart = [...cart];
+        }
+        setCart(newCart);
+    }
+
+    const chooseForMeHandle = (cart) => {
+        let myProducts = cart;
+        let randomItem = myProducts[Math.floor(Math.random() * myProducts.length)];
+        setRandomProduct(randomItem);
+    }
+
+    const resetHandle = () => {
+        let resetCart = [];
+        setCart(resetCart);
+        setRandomProduct(resetCart);
+    }
+
+    const deleteBtn = (carts) => {
+        const deletedItem = carts;
+        let finalCart = [...cart];
+        finalCart.pop(deletedItem);
+        setCart(finalCart);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}
+                    randomProduct={randomProduct}
+                    chooseForMeHandle={chooseForMeHandle}
+                    resetHandle={resetHandle}
+                    deleteBtn={deleteBtn}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
